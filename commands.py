2fff7b4e08ae0b5291eb556093d7de048c6f2d08
m.arestov <m.arestov@estesis.tech>
2024-11-13 18:27:54 +0700

    feat(Bot): Added command about

--- commands.py
+++ commands.py
@@ -17,7 +17,25 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Привет, {update.effective_user.first_name}! Чем могу помочь?')
 
 
-async def handle_message(update: Update, context):
+async def about(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    about_text = (
+        "🤖 *Llama3 Бот* 🤖\n\n"
+        "Этот бот создан на базе мощной нейросети llama3, которая поможет вам находить ответы на сложные вопросы, "
+        "обсуждать разнообразные темы, генерировать идеи и тексты, а также просто вести интересные беседы!\n\n"
+        "*Что бот умеет?*\n"
+        "- Отвечает на любые вопросы\n"
+        "- Помогает решать задачи и обрабатывать данные\n"
+        "- Проводит анализ текстов и генерирует текст по запросу\n"
+        "- Поддерживает продуктивный диалог\n\n"
+        "*Как использовать?*\n"
+        "Просто напишите сообщение, и бот постарается предоставить вам наилучший ответ, исходя из своих возможностей."
+    )
+    photo = open("./res/ollama.png", "rb")
+    await update.message.reply_photo(photo=photo)
+    await update.message.reply_text(text=about_text, parse_mode="Markdown")
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
     if user_id not in user_ids:
         user_ids[user_id] = {'last_message': None, 'preferences': {}}
@@ -27,7 +45,6 @@ async def handle_message(update: Update, context):
     context_messages = context_memory[user_id]
 
     context_messages.append({'role': 'user', 'content': message_text})
-
     context_memory[user_id] = context_messages[-8:]
 
     try:
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import asyncio
 
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
-from commands import handle_message, start
+from commands import handle_message, start, about
 from settings import bot_settings
 
 
@@ -10,6 +10,7 @@ async def main() -> None:
     application = ApplicationBuilder().token(bot_settings.FATHER_TOKEN).build()
 
     application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler("about", about))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     await application.run_polling()
